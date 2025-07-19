e3e9cb773cb126cb33a32f666e637af23cc8f825
davidquintanajob <davidquintanajob@gmail.com>
2025-07-19 11:48:33 -0400

    Add: Midelware from show in console all endpoinds consulte

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { connectDatabase } from './database/database';
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { Task } from './models/Task';
@@ -8,6 +8,12 @@ import taskRoutes from './routs/taskRoutes';
 dotenv.config();
 
 const app = express();
+
+// Middleware para logging de cada consulta
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+});
 
 app.use(express.json());
 app.use(cors({ origin: process.env.CORS_OPTIONS || '*' }));
